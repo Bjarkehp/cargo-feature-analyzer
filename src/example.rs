e6314199f395f301f47a9967f296319a7fc27353
Bjarke Hammerbak Paluszewski <bjarkehp@gmail.com>
2025-03-06 13:12:54 +0100

    additions to example.rs

--- src/example.rs
+++ src/example.rs
@@ -1,7 +1,20 @@
 const GLOBAL: u32 = 1;
 
-#[cfg(feature = "example")]
+#[cfg(flag = "example")]
 fn main() {
     println!("{}", 2 + 2);
     println!("Hello, world!");
+    let x = Example;
+    println!("{:?}", x);
 }
+
+#[derive(Debug, Eq)]
+#[cfg(all(flag = "example", feature))]
+#[cfg_attr(serde, derive(serde::Serialize, serde::Deserialize))]
+struct Example {
+    x: u32,
+    s: String,
+    t: Test
+}
+
+struct Test;
