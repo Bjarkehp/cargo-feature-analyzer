584f00c69c4ae6172fd17e52d6e86506ab6b642a
Bjarke Hammerbak Paluszewski <bjarkehp@gmail.com>
2026-03-04 12:03:40 +0100

    removed unused scripts

--- scripts/generate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cargo run --bin feature-model-generator -- configurations/"$1" models/"$1".uvl --ac-poset models/"$1".acposet.dot
--- scripts/scrape.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p configurations/"$1"
-cargo run --bin feature-configuration-scraper -- "$1" configurations/"$1".toml configurations/"$1"
