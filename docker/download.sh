bd8b8c0ea3005a443fe7fc1464a1f24ad1d4817a
Bjarke Hammerbak Paluszewski <bjarkehp@gmail.com>
2026-04-27 09:23:04 +0200

    database download script now always downloads into the docker directory

--- docker/download.sh
+++ docker/download.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -L "https://drive.usercontent.google.com/download?id=1NERRdYkTfDdmnQsqTALE5elrwG0FWhNY&confirm=yy" -o db-dump.tar.gz
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+curl -L "https://drive.usercontent.google.com/download?id=1NERRdYkTfDdmnQsqTALE5elrwG0FWhNY&confirm=yy" -o "$SCRIPT_DIR/db-dump2.tar.gz"
