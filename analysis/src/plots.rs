78fe4f34c4a14e14a1b8b39b0c8a40d3d0ee585f
Bjarke Hammerbak Paluszewski <bjarkehp@gmail.com>
2026-05-09 20:53:38 +0200

    updated plots to work per crate per config count

--- analysis/src/plots.rs
+++ analysis/src/plots.rs
@@ -11,49 +11,117 @@ fn main() -> anyhow::Result<()> {
     let config = config_from_args(args)?;
     let result = config.result;
     let plot = config.plot;
+    
+    let satisfiability_crate_result = result.join("satisfiability_crate");
+    let satisfiability_crate_plot = plot.join("satisfiability_crate");
+    let satisfiability_result = result.join("satisfiability");
     let satisfiability_plot = plot.join("satisfiability");
+    let satisfiability_wrt_configs = plot.join("satisfiability_wrt_configs");
+    let satisfiability_wrt_features = plot.join("satisfiability_wrt_features");
 
     std::fs::create_dir_all(&plot)?;
     std::fs::create_dir_all(&satisfiability_plot)?;
+    std::fs::create_dir_all(&satisfiability_crate_plot)?;
+    std::fs::create_dir_all(&satisfiability_wrt_configs)?;
+    std::fs::create_dir_all(&satisfiability_wrt_features)?;
 
     let feature_stats = load_map::<FeatureStats>(result.join("feature_stats.csv"))?;
     let declared_stats = load_map::<ModelStats>(result.join("flat_model_stats.csv"))?;
     let fca_stats = load_map::<ModelStats>(result.join("fca_model_stats.csv"))?;
     let line_count_rows = load_map::<LineCountRow>(result.join("line_count.csv"))?;
     let config_stats = load_map::<ConfigStats>(result.join("configuration_stats.csv"))?;
-    let satisfiability_rows = load_map::<SatisfiabilityRow>(result.join("satisfiability.csv"))?;
 
     feature_stats::plot(&feature_stats, plot.join("feature_stats.png"))?;
     features_and_dependencies::plot(&feature_stats, plot.join("features_and_dependencies.png"))?;
     declared_vs_fca::plot(&declared_stats, &fca_stats, plot.join("declared_vs_fca.png"))?;
     cross_tree_constraints::plot(&declared_stats, &fca_stats, plot.join("cross_tree_constraints.png"))?;
     line_count_and_features::plot(&line_count_rows, &feature_stats, plot.join("line_count_and_features.png"))?;
+
     if !config_stats.is_empty() {
         default_configs::plot(&config_stats, plot.join("default_configs.png"))?;
         distinct_configs::plot(&config_stats, plot.join("unique_configs.png"))?;
     }
+
     
-    let satisfiability_path = result.join("satisfiability");
-    let satisfiablity_table_paths = std::fs::read_dir(&satisfiability_path)
+
+    let satisfiability_crate_table_results = std::fs::read_dir(&satisfiability_crate_result)
+        .with_context(|| "Cannot find directory for satisfiability tables")?
+        .map_ok(|e| e.path())
+        .collect::<Result<Vec<_>, _>>()
+        .with_context(|| "Error while listing entries in satisfiability_crate directory")?
+        .into_iter()
+        .map(std::fs::read_dir)
+        .collect::<std::io::Result<Vec<_>>>()?
+        .into_iter()
+        .flatten()
+        .map_ok(|e| e.path())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    for path in satisfiability_crate_table_results {
+        let file_name = path
+            .file_name()
+            .expect("File has a name")
+            .to_string_lossy();
+        let file_stem = file_name
+            .rsplit_once('.')
+            .with_context(|| format!("Path {path:?} has no extension in name"))?
+            .0;
+
+        let config_count = path
+            .parent()
+            .expect("Path must have a parent")
+            .file_name()
+            .expect("Directory is not root")
+            .to_string_lossy()
+            .parse::<usize>()
+            .with_context(|| format!("Directory {path:?} has a non-integer name"))?;
+
+        let rows = load_vec::<SatisfiabilityCrateRow>(&path)?;
+
+        let parent_plot_dir = satisfiability_crate_plot.join(format!("{config_count}"));
+
+        std::fs::create_dir_all(&parent_plot_dir)?;
+
+        let plot_path = parent_plot_dir
+            .join(file_stem)
+            .with_added_extension("png");
+
+        satisfiability_crate::plot(&rows, plot_path, file_stem)?;
+    }
+
+    let satisfiability_table_paths = std::fs::read_dir(&satisfiability_result)
         .with_context(|| "Cannot find directory for satisfiability tables")?
         .map_ok(|e| e.path())
         .collect::<Result<Vec<_>, _>>()
         .with_context(|| "Error while listing entries in satisfiability directory")?;
 
-    for path in satisfiablity_table_paths {
-        let name = path.file_stem()
-            .expect("File has a name")
-            .to_string_lossy();
-        let rows = load_vec::<SatisfiabilityCrateRow>(&path)?;
-        let plot_path = satisfiability_plot
-            .join(name.as_ref())
-            .with_extension("png");
-        satisfiability_crate::plot(&rows, plot_path, &name)?;
-    }
+    for path in satisfiability_table_paths {
+        let rows = load_map::<SatisfiabilityRow>(&path)?;
 
-    satisfiability::plot(&satisfiability_rows, plot.join("satisfiability.png"))?;
-    satisfiability_wrt_configs::plot(&satisfiability_rows, &config_stats, plot.join("satisfiability_wrt_configs.png"))?;
-    satisfiability_wrt_features::plot(&satisfiability_rows, &feature_stats, plot.join("satisfiability_wrt_features.png"))?;
+        let configs = path.file_stem()
+            .expect("File has a name")
+            .to_string_lossy()
+            .parse::<usize>()
+            .with_context(|| format!("File {path:?} has a non-integer name"))?;
+
+        let plot_path = satisfiability_plot
+            .join(format!("{configs}"))
+            .with_extension("png");
+
+        satisfiability::plot(&rows, &plot_path)?;
+
+        let plot_path = satisfiability_wrt_configs
+            .join(format!("{configs}"))
+            .with_extension("png");
+
+        satisfiability_wrt_configs::plot(&rows, &config_stats, &plot_path)?;
+
+        let plot_path = satisfiability_wrt_features
+            .join(format!("{configs}"))
+            .with_extension("png");
+
+        satisfiability_wrt_features::plot(&rows, &feature_stats, &plot_path)?;
+    }
 
     Ok(())
 }
